f9b898cca901ce6728ded1c5db911e4f9b6ebe69
ThomasWWebb <thomaswebb98uk@gmail.com>
2019-01-24 16:48:19 +0000

    Adds calendar with some example events, this does come with an error warning which I'll look into

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ app.use(express.static('public'));
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }))
 
+var events = [{
+    title: 'Event 1',
+    start: '2019-01-24T13:13:55.008',
+    end: '2019-01-24T14:14:55.008'
+    },
+    {
+    title: 'Event 2',
+    start: '2019-01-25T15:13:55',
+    end: '2019-01-25T16:13:55'
+    }];
+
+app.get('/events', function(req, resp){
+    resp.send(events);
+}) 
 
 
 app.listen(1010);
