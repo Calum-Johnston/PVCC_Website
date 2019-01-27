d76c02a85c09979925b41c80ea9a03a150a78da0
ThomasWWebb <thomaswebb98uk@gmail.com>
2019-01-27 19:30:12 +0000

    Adds choosing a facility and returns the appropriate calendar and adds a booking button

--- index.js
+++ index.js
@@ -6,19 +6,42 @@ app.use(express.static('public'));
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }))
 
-var events = [{
+var astroturfEvents = [{
     title: 'Event 1',
-    start: '2019-01-24T13:13:55.008',
-    end: '2019-01-24T14:14:55.008'
+    start: '2019-01-27T13:13:55.008',
+    end: '2019-01-27T14:14:55.008'
     },
     {
     title: 'Event 2',
-    start: '2019-01-25T15:13:55',
-    end: '2019-01-25T16:13:55'
-    }];
+    start: '2019-01-27T15:13:55',
+    end: '2019-01-27T16:13:55'
+    }
+];
 
-app.get('/events', function(req, resp){
-    resp.send(events);
+var footballpitchEvents = [{
+    title: 'Football Pitch event 1',
+    start: '2019-01-28T13:11:55.008',
+    end: '2019-01-28T14:14:55.008'
+    },
+    {
+    title: 'Football Pitch event 2',
+    start: '2019-01-29T15:13:55',
+    end: '2019-01-29T18:13:55'
+    }
+];
+
+var performingartsEvents = [{}];
+var theatreEvents = [{}];
+var itsuiteEvents = [{}];
+var classroomEvents = [{}];
+var dininghallEvents = [{}];
+
+
+var events = {"astroturf" : astroturfEvents, "footballpitch" : footballpitchEvents, "performingarts" : performingartsEvents, "theatre" : theatreEvents, "itsuite" : itsuiteEvents, "classroom" : classroomEvents, "dininghall" : dininghallEvents}
+
+
+app.get('/events/:room', function(req, resp){
+    resp.send(events[req.params.room]);
 }) 
 
 
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -14,32 +14,3 @@ $(function(){
   $("#header").load("includes/header.html"); 
   $("#footer").load("includes/footer.html"); 
 });
-
-//calendar functions
-
-$('#calendar').fullCalendar({
-    columnFormat: 'ddd D/M',
-    defaultView: 'agendaWeek',
-    height: 440,
-    contentHeight:420,
-    header: {
-        left: 'title',
-        right: 'prev,next today '
-    },
-
-    titleFormat: '[Room]',
-    allDaySlot: false,
-    businessHours:
-        {
-                start: '08:00',
-                end:   '22:30',
-                dow: [ 0, 1, 2, 3, 4, 5, 6]
-        },
-    events: {
-        url: "http://127.0.0.1:1010/events",
-    },
-    eventBackgroundColor: "rgb(255, 201, 45)",
-    eventTextColor: "black",
-    eventBorderColor:"rgb(255, 201, 45)"
-});
-
