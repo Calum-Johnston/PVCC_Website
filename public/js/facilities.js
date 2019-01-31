c6181a27b6b39ba07771dedd732cde1922abb36f
samwinder900 <samwinder900@gmail.com>
2019-01-31 18:17:26 +0000

    Added missing photos

---
+++ public/js/facilities.js
@@ -0,0 +1,65 @@
+//load facilities
+$(function(){    
+    $.get("/facilities", function(data){
+        $("#facilities .request-result").html(" ");
+        //alert(data);
+        for (i = 0; i < data.length; i++){
+            if (data[i] == undefined){
+                break
+            }
+            
+            var roomId = data[i].roomId
+            var imagePath = data[i].roomImage
+            var roomName = data[i].roomName
+            var roomDescription = data[i].roomDescription
+            var roomId = data[i].roomId
+            
+            //Change path to filler image if none is assigned and provide full path if it is
+            if (imagePath == ""){
+                imagePath = "facilities/room.jpg"
+            } else {
+                imagePath = "facilities/" + imagePath
+            }
+            
+            //convert room description into a preview of the description text of 120 characters to the nearest word.
+            roomDescription = roomDescription.substring(0,roomDescription.lastIndexOf(" ",120)) + "..."
+            
+            
+            //$("#facilities .request-result").append('<div class="col-12 col-md-6 col-lg-4 mb-4"><div class="card"><img class="card-img-top" src="img/'+imagePath+'" alt="Card image cap"><div class="card-body"><h5 class="card-title">'+roomName+'</h5><p class="card-text">'+roomDescription+'</p><a id="'+roomId+'" class="btn btn-primary facilityLink">Go somewhere</a></div></div></div>')
+            
+            $('<div class="col-12 col-md-6 col-lg-4 mb-4"><div class="card"><img class="card-img-top" src="img/'+imagePath+'" alt="Card image cap"><div class="card-body"><h5 class="card-title">'+roomName+'</h5><p class="card-text">'+roomDescription+'</p><a id="'+roomId+'" class="btn btn-primary facilityLink">Go somewhere</a></div></div></div>').hide().appendTo("#facilities .request-result").fadeIn(600)
+        
+        }
+        
+
+    });
+
+
+    
+});
+
+$("#facilities .request-result").on("click", ".facilityLink", function(){
+    var facilityId = $(this).attr("id")
+    $.get("/facilities/"+facilityId, function(data){
+        console.log(data)
+        var roomId = data[0].roomId
+        var imagePath = data[0].roomImage
+        var roomName = data[0].roomName
+        var roomDescription = data[0].roomDescription
+        var roomId = data[0].roomId
+        
+        //Change path to filler image if none is assigned and provide full path if it is
+        if (imagePath == ""){
+            imagePath = "img/facilities/room.jpg"
+        } else {
+            imagePath = "img/facilities/" + imagePath
+        }
+        
+        $("#facilities .request-result").html(" ");
+        $("#facilities .request-result").append("<img class='col-6' src=" + imagePath + " />")
+        $("#facilities .request-result").append("<div class='col-6'><h2>" + roomName + "</h2><p>" + roomDescription + "</p></div>")
+
+    });
+});
+
+
---
+++ public/js/homepage.js
@@ -0,0 +1,68 @@
+//load facilities
+$(function(){    
+    $.get("/facilities", function(data){
+        $("#facilities .request-result").html(" ");
+        //alert(data);
+        for (i = 0; i < 6; i++){
+            if (data[i] == undefined){
+                break
+            }
+            
+            var roomId = data[i].roomId
+            var imagePath = data[i].roomImage
+            var roomName = data[i].roomName
+            var roomDescription = data[i].roomDescription
+            var roomId = data[i].roomId
+            
+            //Change path to filler image if none is assigned and provide full path if it is
+            if (imagePath == ""){
+                imagePath = "facilities/room.jpg"
+            } else {
+                imagePath = "facilities/" + imagePath
+            }
+            
+            //convert room description into a preview of the description text of 120 characters to the nearest word.
+            roomDescription = roomDescription.substring(0,roomDescription.lastIndexOf(" ",120)) + "..."
+            
+            
+            $("#facilities .request-result").append('<div class="col-12 col-md-6 col-lg-4 mb-4"><div class="card"><img class="card-img-top" src="img/'+imagePath+'" alt="Card image cap"><div class="card-body"><h5 class="card-title">'+roomName+'</h5><p class="card-text">'+roomDescription+'</p><a href="#" class="btn btn-primary">Go somewhere</a></div></div></div>')
+        
+        }
+        
+
+    });
+    
+    //load what's on  
+    $.get("/classes", function(data){
+        $("#classes .request-result").html(" ");
+        //alert(data);
+        for (i = 0; i < 6; i++){
+            if (data[i] == undefined){
+                break
+            }
+
+            var roomId = data[i].roomId
+            var imagePath = data[i].roomImage
+            var roomName = data[i].roomName
+            var roomDescription = data[i].roomDescription
+            var roomId = data[i].roomId
+
+            //Change path to filler image if none is assigned and provide full path if it is
+            if (imagePath == ""){
+                imagePath = "facilities/room.jpg"
+            } else {
+                imagePath = "facilities/" + imagePath
+            }
+
+            //convert room description into a preview of the description text of 120 characters to the nearest word.
+            roomDescription = roomDescription.substring(0,roomDescription.lastIndexOf(" ",120)) + "..."
+
+
+            $("#facilities .request-result").append('<div class="col-12 col-md-6 col-lg-4 mb-4"><div class="card"><img class="card-img-top" src="img/'+imagePath+'" alt="Card image cap"><div class="card-body"><h5 class="card-title">'+roomName+'</h5><p class="card-text">'+roomDescription+'</p><a href="#" class="btn btn-primary">Go somewhere</a></div></div></div>')
+
+        }
+
+
+    });
+});
+
